417d0b2320a160da027aede68db45e763a293368
iscipar <iscipar@hotmail.com>
2023-06-23 14:22:26 +0200

    Se crean Cypress tests para las stories de la librería shared-components

--- apps/shared-components-e2e/src/e2e/header/header.component.cy.ts
+++ apps/shared-components-e2e/src/e2e/header/header.component.cy.ts
@@ -1,6 +1,14 @@
 describe('shared-components', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=headercomponent--primary'));
-  it('should render the component', () => {
-    cy.get('learn-nx-header').should('exist');
+  it('should not be signed', () => {
+    cy.visit('/iframe.html?id=headercomponent--not-signed')
+    cy.get('#home-button').should('not.exist');
+    cy.get('#login-button').should('exist');
+    cy.get('#logout-button').should('not.exist');
+  });
+  it('should be signed', () => {
+    cy.visit('/iframe.html?id=headercomponent--signed')
+    cy.get('#home-button').should('exist');
+    cy.get('#login-button').should('not.exist');
+    cy.get('#logout-button').should('exist');
   });
 });
